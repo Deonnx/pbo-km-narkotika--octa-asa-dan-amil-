4fe2059f2a076e64e5b697425519c000415b2e41
Deonnx <152576051+Deonnx@users.noreply.github.com>
2026-06-09 23:22:07 +0700

    + kontroler(half)

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pbo
 tugasakhir
+
+
+### 1. Layer MODEL (Dikerjakan oleh: asa)
+* **Fokus Direktori:** `src/main/java/model/`
+* **Tugas:** Membuat struktur data, relasi, dan perhitungan.
+
+* **File yang dikelola:** `Putusan.java`, `KnowledgeRepository.java`, `StatistikPutusan.java`.
+
+### 2. Layer VIEW (Dikerjakan oleh: amil)
+* **Fokus Direktori:** `src/main/java/view/` & `src/main/resources/com/example/pbo/`
+* **Tugas:** Membuat tampilan antarmuka (Console atau JavaFX) dan menerima input *user*.
+* **File yang dikelola:** `ConsoleView.java`, `JavaFXView.fxml`, (dan file CSS jika ada).
+
+### 3. Layer CONTROLLER (Dikerjakan oleh: Octa)
+* **Fokus Direktori:** `src/main/java/controller/`, `src/main/java/util/`, `src/main/java/app/`
+* **Tugas:** Menjadi jembatan antara Model dan View, validasi input (try-catch), dan mengatur alur program di `Main.java`.
+* **File yang dikelola:** `KnowledgeController.java`, `InputHandler.java`, `Main.java`.
--- src/main/java/controller/KnowledgeController.java
+++ src/main/java/controller/KnowledgeController.java
@@ -1,4 +1,28 @@
-package controller;
+package com.example.pbo.controller;
+
+import com.example.pbo.model.KnowledgeRepository;
+import com.example.pbo.model.Putusan;
+import com.example.pbo.model.StatistikPutusan;
+import java.util.ArrayList;
 
 public class KnowledgeController {
-}
+    private KnowledgeRepository repository;
+
+    public KnowledgeController(KnowledgeRepository repository) {
+        this.repository = repository;
+    }
+
+    public boolean tambahPutusan(String[] data) {
+        try {
+            int umur = Integer.parseInt(data[4]);
+            double berat = Double.parseDouble(data[6]);
+            int hukuman = Integer.parseInt(data[9]);
+            double denda = Double.parseDouble(data[10]);
+
+            Putusan p = new Putusan(data[0], data[1], data[2], data[3], umur, data[5], berat, data[7], data[8], hukuman, denda, data[11]);
+            repository.simpan(p);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
