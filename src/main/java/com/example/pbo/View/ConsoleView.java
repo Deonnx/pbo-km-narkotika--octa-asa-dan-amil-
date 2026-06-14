57832de9a2c978ddf85e8f755ebbd5ba6f419d51
Nullspecter <amiluddin9410@gmail.com>
2026-06-14 19:33:51 +0700

    add: implementasi menu utama di ConsoleView

--- src/main/java/com/example/pbo/View/ConsoleView.java
+++ src/main/java/com/example/pbo/View/ConsoleView.java
@@ -1,4 +1,17 @@
 package com.example.pbo.View;
 
 public class ConsoleView {
+
+    public int tampilkanMenu() {
+        System.out.println("\n=== KMS PUTUSAN NARKOTIKA ===");
+        System.out.println("1. Tambah Putusan");
+        System.out.println("2. Tampilkan Semua");
+        System.out.println("3. Cari Putusan");
+        System.out.println("4. Filter Putusan");
+        System.out.println("5. Statistik");
+        System.out.println("6. Hapus Putusan");
+        System.out.println("0. Keluar");
+
+        return 0;
+    }
 }
