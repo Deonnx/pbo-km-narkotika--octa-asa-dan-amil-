deec71744b8af365e81c3ba0c24037038d440bc6
Deonnx <152576051+Deonnx@users.noreply.github.com>
2026-06-23 20:55:59 +0700

    penambahan dan perbaikan fungsi di main.java

--- src/main/java/com/example/pbo/App/Main.java
+++ src/main/java/com/example/pbo/App/Main.java
@@ -1,4 +1,22 @@
 package com.example.pbo.App;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/example/pbo/JavaFXView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Knowledge Management System - Putusan Pengadilan Narkotika");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
