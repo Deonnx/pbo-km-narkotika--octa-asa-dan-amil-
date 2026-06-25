e6ad6b4c36ff91561c87a777de9d92756a54ff37
Nullspecter <amiluddin9410@gmail.com>
2026-06-25 20:54:48 +0700

    pembuatan class JavaFXController.java

---
+++ src/main/java/com/example/pbo/View/JavaFXController.java
@@ -0,0 +1,17 @@
+package com.example.pbo.View;
+
+import com.example.pbo.controller.KnowledgeController;
+import com.example.pbo.model.Putusan;
+import com.example.pbo.model.KnowledgeRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class JavaFXController {
+
+}
