d72c67b724ce8add9c91d8598e70a131aa992ce5
Nullspecter <amiluddin9410@gmail.com>
2026-06-25 20:56:10 +0700

    upgrade JavaFXController.java

--- src/main/java/com/example/pbo/View/JavaFXController.java
+++ src/main/java/com/example/pbo/View/JavaFXController.java
@@ -14,4 +14,75 @@ import javafx.collections.ObservableList;
 
 public class JavaFXController {
 
+    @FXML
+    private TextField txtNomor;
+
+    @FXML
+    private TextField txtNama;
+
+    @FXML
+    private TextField txtJenis;
+
+    @FXML
+    private TextField txtVonis;
+
+    @FXML
+    private TextField txtDenda;
+
+    @FXML
+    private TableView<Putusan> tablePutusan;
+
+    @FXML
+    private TableColumn<Putusan, String> colNomor;
+
+    @FXML
+    private TableColumn<Putusan, String> colNama;
+
+    @FXML
+    private TableColumn<Putusan, String> colJenis;
+
+    @FXML
+    private TableColumn<Putusan, Integer> colVonis;
+
+    @FXML
+    private TableColumn<Putusan, Double> colDenda;
+
+    private KnowledgeController controller;
+
+    private void refreshTable() {
+
+        ObservableList<Putusan> data = FXCollections.observableArrayList(controller.getDaftarSemua());
+        tablePutusan.setItems(data);
+    }
+
+    private void clearForm() {
+        txtNomor.clear();
+        txtNama.clear();
+        txtJenis.clear();
+        txtVonis.clear();
+        txtDenda.clear();
+    }
+
+    @FXML
+    public void initialize() {
+
+        controller = new KnowledgeController(new KnowledgeRepository());
+
+        colNomor.setCellValueFactory(
+                new PropertyValueFactory<>("nomorPerkara"));
+
+        colNama.setCellValueFactory(
+                new PropertyValueFactory<>("namaTerdakwa"));
+
+        colJenis.setCellValueFactory(
+                new PropertyValueFactory<>("jenisNarkotika"));
+
+        colVonis.setCellValueFactory(
+                new PropertyValueFactory<>("vonisHukuman"));
+
+        colDenda.setCellValueFactory(
+                new PropertyValueFactory<>("vonisDenda"));
+
+        refreshTable();
+    }
 }
