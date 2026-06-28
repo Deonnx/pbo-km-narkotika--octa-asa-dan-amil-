41e8111ef8421700356a0b32521dcdd7bc0fd333
Nullspecter <amiluddin9410@gmail.com>
2026-06-28 09:26:46 +0700

    penambahan method cariPutusan dan updatePutusan

--- src/main/java/com/example/pbo/model/KnowledgeRepository.java
+++ src/main/java/com/example/pbo/model/KnowledgeRepository.java
@@ -12,6 +12,24 @@ public class KnowledgeRepository {
     public void simpan(Putusan p) { daftarPutusan.add(p); }
     public ArrayList<Putusan> getDaftarSemua() { return daftarPutusan; }
 
+    public ArrayList<Putusan> cariPutusan(String keyword) {
+
+        ArrayList<Putusan> hasil = new ArrayList<>();
+
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return daftarPutusan;
+        }
+
+        keyword = keyword.toLowerCase();
+
+        for (Putusan p : daftarPutusan) {
+            if (p.getNomorPerkara().toLowerCase().contains(keyword)
+                    || p.getNamaTerdakwa().toLowerCase().contains(keyword)) {hasil.add(p);}
+        }
+
+        return hasil;
+    }
+
     private void muatDataAwal() {
         simpan(new Putusan("1/Pid/2024", "Andi", "Sabu", 60, 1000000000));
         simpan(new Putusan("2/Pid/2024", "Budi", "Ganja", 12, 0));
@@ -31,16 +49,30 @@ public class KnowledgeRepository {
         return false;
     }
 
-    public void updatePutusan(String nomor, String nama, String jenis, int vonis, double denda) {
+    public void updatePutusan(String nomor,
+                              String nama,
+                              String jenis,
+                              int vonis,
+                              double denda) {
 
-        for (Putusan p : daftarPutusan) {
+        for (int i = 0; i < daftarPutusan.size(); i++) {
+
+            Putusan p = daftarPutusan.get(i);
 
             if (p.getNomorPerkara().equals(nomor)) {
 
-                daftarPutusan.remove(p);
-                daftarPutusan.add(new Putusan(nomor, nama, jenis, vonis, denda));
+                daftarPutusan.set(
+                        i,
+                        new Putusan(
+                                nomor,
+                                nama,
+                                jenis,
+                                vonis,
+                                denda
+                        )
+                );
 
-                break;
+                return;
             }
         }
     }
