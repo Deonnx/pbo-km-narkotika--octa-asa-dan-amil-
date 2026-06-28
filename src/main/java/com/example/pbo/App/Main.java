2abc892b6bf6f25a1cdcdab6ed470156c5a1d461
Nullspecter <amiluddin9410@gmail.com>
2026-06-28 09:25:21 +0700

    penyesuaian kecil pada Main.java

--- src/main/java/com/example/pbo/App/Main.java
+++ src/main/java/com/example/pbo/App/Main.java
@@ -10,9 +10,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/example/pbo/JavaFXView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 850);
         stage.setTitle("Knowledge Management System - Putusan Pengadilan Narkotika");
         stage.setScene(scene);
+        stage.setMinWidth(1200);
+        stage.setMinHeight(750);
+        stage.setMaximized(true);
         stage.show();
     }
 
